2e976c4e19e5a09faf99e4dc6c4efc4549647643
Lluis Gili <kovacks@gmail.com>
2017-01-18 11:40:20 +0100

    consolidatedvars

--- roles/installacsman/files/getinitialkeys.sh
+++ roles/installacsman/files/getinitialkeys.sh
@@ -13,9 +13,9 @@ if [[ -z $APIKEY ]]; then
     #echo "keys not set. Create"
     APIKEY=`cloudmonkey -c $CONFIGFILE register userkeys id=${ADMINID} | grep ^apikey | awk '{print $3}'`
     SECRETKEY=`cloudmonkey -c $CONFIGFILE list users admin filter=secretkey | grep ^secretkey | awk '{print $3}'`
-    echo "$APIKEY:$SECRETKEY"
+    echo "$APIKEY:$SECRETKEY:$ADMINID"
 else
     #echo "keys exist"
     SECRETKEY=`cloudmonkey -c $CONFIGFILE list users admin filter=secretkey | grep ^secretkey | awk '{print $3}'`
-    echo "$APIKEY:$SECRETKEY"
+    echo "$APIKEY:$SECRETKEY:$ADMINID"
 fi
